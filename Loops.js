e7364fe265d32796108d62f65a2a8bda5d528725
unknown <cabdallaadam@gmail.com>
2025-11-06 06:11:47 -0800

    change

--- Loops.js
+++ Loops.js
@@ -1,36 +1,61 @@
-// for loop
+// // for loop
 
-for (i=1;i<=5;i++){
-    console.log(i)
-}
+// for (i=1;i<=5;i++){
+//     console.log(i)
+// }
     
 
-// while loop
+// // while loop
 
-let i = 1
+// let i = 1
 
-while(i>=5){
-    console.log(i)
-    i++
-}
+// while(i>=5){
+//     console.log(i)
+//     i++
+// }
 
-let correctpassword = "abdi123"
-let attempts = 0
-let isloggin = false
+  //system make while loop in three ways
 
-while (attempts < 3 & !isloggin){
-    let userpasswrd = prompt("enter you password: ")
-    if (userpasswrd == correctpassword){
-        console.log("loging seccessfully")
-        isloggin = true
-    }else{
-        attempts++,
-        console.log("incorect password you have",3 - attempts, "atttempts letter")
+// let correctpassword = "abdi123"
+// let attempts = 0
+// let isloggin = false
+
+// while (attempts < 3 & !isloggin){
+//     let userpasswrd = prompt("enter you password: ")
+//     if (userpasswrd == correctpassword){
+//         console.log("loging seccessfully")
+//         isloggin = true
+//     }else{
+//         attempts++,
+//         console.log("incorect password you have",3 - attempts, "atttempts letter")
+//     }
+// }    
+    
+
+
+// if (!isloggin){
+//     console.log("sorry you lose 3 attemps pls try again leter")
+// }
+
+// let number;
+
+// do{
+
+//     number=prompt("enter a number")
+// }while(isNaN(number)){
+
+//     // console.log("not defined")
+// }
+// console.log("yo entered the numer is :",number)
+
+for( let i=1;i<=10;i++){
+    if (i==9){
+        break;
     }
-}    
-    
 
-
-if (!isloggin){
-    console.log("sorry you lose 3 attemps pls try again leter")
+    console.log(i)
 }
+
+
+
+
